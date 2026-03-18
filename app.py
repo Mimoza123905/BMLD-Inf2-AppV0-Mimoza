355dcb9d65b00a34c8ccb7c39c1838f2bae47e88
Mimoza <mehmemim@students.zhaw.ch>
2026-03-18 10:10:46 +0100

    app.py

--- app.py
+++ app.py
@@ -4,7 +4,27 @@ st.set_page_config(page_title="Meine App", page_icon=":material/home:")
 
 pg_home = st.Page("views/home.py", title="Home", icon=":material/home:", default=True)
 pg_second = st.Page("views/unterseite_a.py", title="Unterseite A", icon=":material/info:")
-pg_ph = st.Page("views/ph.py", title="pH Rechner", icon=":material/science:")
+pg_ph = st.Page("views/pH-Rechner.py", title="pH Rechner", icon=":material/science:")
 
-pg = st.navigation([pg_home, pg_second, pg_add, pg_ph])
+pg = st.navigation([pg_home, pg_second, pg_ph])
 pg.run()
+# --- NEW CODE: import and initialize data manager and login manager ---
+from utils.data_manager import DataManager
+from utils.login_manager import LoginManager
+
+data_manager = DataManager(       # initialize data manager
+    fs_protocol='webdav',         # protocol for the filesystem, use webdav for switch drive
+    fs_root_folder="BMLD_App_DB"  # folder on switch drive where the data is stored
+    ) 
+login_manager = LoginManager(data_manager) # handles user login and registration
+login_manager.login_register()             # stops if not logged in
+# --- END OF NEW CODE ---
+
+# --- CODE UPDATE: load user data from data manager if not already present in session state --
+if 'data_df' not in st.session_state:
+    st.session_state['data_df'] = data_manager.load_user_data(
+        'data.csv',                     # The file on switch drive where the data is stored
+        initial_value=pd.DataFrame(),   # Initial value if the file does not exist
+        parse_dates=['timestamp']       # Parse timestamp as datetime
+    )
+# --- END OF CODE UPDATE ---
